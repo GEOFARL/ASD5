5af668e7819b2bc9a0ed561918388e91903cdd77
Maxym Tsyganok <toper.one111@gmail.com>
2023-04-28 08:49:55 +0300

    Add max-hepaify function

--- heaps.py
+++ heaps.py
@@ -9,3 +9,16 @@ def left(i):
 
 def right(i):
   return 2 * i + 1
+
+def maxHeapify(A, i, N):
+  p = left(i)
+  q = right(i)
+  if p < N and A[p] > A[i]:
+    largest = p
+  else:
+    largest = i
+  if q < N and A[q] > A[largest]:
+    largest = q
+  if largest != i:
+    [A[i], A[largest]] = [A[largest], A[i]]
+    maxHeapify(A, largest, N)
